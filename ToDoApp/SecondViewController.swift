2a821b02e8bfa062a6139cd96a71714d996c364f
Plawan Kumar Rath <plawanrath@gmail.com>
2015-01-25 14:38:10 -0600

    Initial Commit

---
+++ ToDoApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ToDoApp
+//
+//  Created by Plawan Rath on 25/01/15.
+//  Copyright (c) 2015 Plawan Rath. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
